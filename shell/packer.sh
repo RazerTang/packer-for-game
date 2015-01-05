27442c1b1762c28d7f6fe20f365555ca86bd7bd0
Razer752 <razer751@163.com>
2015-01-05 13:04:11 +0800

    modify shell script

--- shell/packer.sh
+++ shell/packer.sh
@@ -1,20 +1,59 @@
 #!/bin/sh
 
 function printUsage(){
-	echo "-png8 create png8 \n"
-	echo "-jsc crate jsc file \n"
-	echo "-texturepacker \n"
-	echo "-icons create icons for cocos2dx\n"
-	echo "-ccbi convert ccb to ccbi\n"
-	echo "-svn commit files for svn\n"
-	echo "-input input folder or file\n"
-	echo "-output output folder or file\n"
-	echo "-help show help \n"
-	echo "-verbose show verbose \n"
+	echo "-p png8 create png8"
+	echo "-j jsc crate jsc file"
+	echo "-t texturepacker"
+	echo "-c icons create icons for cocos2dx"
+	echo "-b ccbi convert ccb to ccbi"
+	echo "-s svn commit files for svn"
+	echo "-i input input folder or file"
+	echo "-o output output folder or file"
+	echo "-h help show help"
+	echo "-v verbose show verbose"
+	echo "-f format  only use for -t,RGBA8888 RGBA4444 RGBA565"
 }
+g_func="" #1 png8 2 jsc 3 texturepacker 4 icons 5 ccbi 6 svn
+g_input=""
+g_output=""
+g_verbose=0 #0 not show verbose, 1 show verbose
+g_format=""
+while getopts "pjtcbshvi:o:f:t::" arg
+do
+	case $arg in
+		h) printUsage;;
+		v) g_verbose=1;;
+		p) g_func=1;;
+		i) g_input=$OPTARG;;
+		o) g_output=$OPTARG;;
+		j) g_func=2;;
+		t) g_func=3;;
+		c) g_func=4;;
+		b) g_func=5;;
+		s) g_func=6;;
+		f) g_format=$OPTARG;;
+	esac
+done
 
-local func
-local input
-local output
+echo func=$g_func
+echo input=$g_input
+echo output=$g_output
+echo foramt=$g_format
+
+if [ $g_func = 1 ]; then 
+	sh ./tools/png8.sh $g_input $g_output
+elif [ $g_func = 2 ]; then
+	sh ./tools/jsc.sh $g_input $g_output
+elif [ $g_func = 3 ]; then
+	sh ./tools/imagepacker.sh $g_input $g_output $g_format
+elif [ $g_func = 4 ]; then
+	sh ./tools/icon.sh $g_input $g_output
+elif [ $g_func = 5 ]; then
+	sh ./tools/ccbi.sh $g_input $g_output
+elif [ $g_func = 6 ]; then
+	sh ./tools/svnci.sh $g_input 
 
 
+
+
+fi
--- shell/tools/imagepacker.sh
+++ shell/tools/imagepacker.sh
@@ -7,14 +7,20 @@
 #  Copyright (c) 2014年 Razer. All rights reserved.
 
 #
-#@param 1 format index RGBA8888
-#@param 2 output plist path
-#@param 3 output image path
-#@param 4 plist prefix aa/aa.png
-#@param 5 input image path
-#
-formats=("RGBA8888" "RGBA4444" "RGB565")
-format=${formats[$1]}
+#@param 1 input  path
+#@param 2 output  path
+#@param 3 format  RGBA8888
+
+#formats=("RGBA8888" "RGBA4444" "RGB565")
+format=$3
+folder=${1##*/}
+outputsheet=$2/$folder.png
+outputdata=$2/$folder.plist
+echo $outputsheet
+echo $outputdata
+echo folder=$folder
+echo $format
+
 tp="/usr/local/bin/TexturePacker"
 arguments="\
 --smart-update \
@@ -26,10 +32,10 @@ arguments="\
 --shape-padding 2 \
 --border-padding 2 \
 --opt $format \
---data $2 \
---sheet $3 \
---replace ^=$4/ \
-$5/*.png"
+--data $outputdata \
+--sheet $outputsheet \
+--replace ^=$folder/ \
+$1/*.png"
 
 echo $arguments
 $tp $arguments
